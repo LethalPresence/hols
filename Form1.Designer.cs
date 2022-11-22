c686a727cd871e4fbe735dafff5489e7a240c6ba
Илалетдинов Азамат <Ilaletdinov666@outlook.com>
2022-11-22 19:57:01 +0500

    wer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,16 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -97,17 +107,107 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(397, 122);
+            this.label2.Location = new System.Drawing.Point(397, 125);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(98, 13);
             this.label2.TabIndex = 6;
             this.label2.Text = "Кол-во операндов";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(506, 148);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(352, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Кол-во простых операторов";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(506, 174);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(352, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Кол-во простых операторов";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(392, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Длинна программы";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(450, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Словарь";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(506, 200);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(506, 226);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(395, 255);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Расчёт по формуле";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(506, 252);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 16;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -132,6 +232,16 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -29,20 +29,32 @@ namespace hols
             textBox1.Text = fileText;
         }
 
-        string[] Arr1 = new string[] { "string", "real", "integer", "for", "boolean", ":=", "var", "begin", "end", "foreach", "while", "repeat", "if", "case", };
+        string[] Arr1 = new string[] { "string", "real", "integer", "for", "boolean", "var", "begin", "end", "foreach", "while", "repeat", "if", "then", "else", "case" };
         string[] Arr2 = new string[] { "@" , "not", "^" , "*" , "/" , "div", "mod", "and", "shl", "shr", "+" , "-" , "or", "xor", "=" , ">" , "<" , "<>" , "<=" , ">=" , "as" , "is" , "in" };
-        char[] Arr3 = new char[] { ';'};
+        char[] Arr3 = new char[] { ';', ' ', '\t', '\n' };
+        string[] prost_oper = new string[] { ":=", "goto"};
+        string[] prost_operand = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
         int oper = 0;
         int operand = 0;
+        int pr_operator = 0;
+        int pr_operand = 0;
+        Double D = 0;
+        Double L = 0;
 
         private void button2_Click(object sender, EventArgs e)
-        { 
+        {
+            oper = 0;
+            operand = 0;
+            pr_operator = 0;
+            pr_operand = 0;
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
             if (textBox1.Text.Length == 0)
                 MessageBox.Show("Ничего не введено!");
             else
             {
-                oper = 0;
-                operand = 0;
                 string[] tex = textBox1.Text.Split(Arr3);
                 for (int i = 0; i < Arr1.Length; i++)
                     foreach (string A in tex)
@@ -51,18 +63,43 @@ namespace hols
                             oper++;
                         }
                 for (int i = 0; i < Arr2.Length; i++)
-                    foreach (string B in tex)
-                        if (B.Contains(Arr2[i]))
+                    foreach (string A in tex)
+                        if (A.Contains(Arr2[i]))
                         {
                             operand++;
+                        }
+                for (int i = 0; i < prost_oper.Length; i++)
+                    foreach (string A in tex)
+                        if (A.Contains(prost_oper[i]))
+                        {
+                            pr_operator++;
+                        }
+                for (int i = 0; i < prost_operand.Length; i++)
+                    foreach (string A in tex)
+                        if (A.Contains(prost_operand[i]))
+                        {
+                            pr_operand++;
                         }
                 Huynya();
             }
         }
         private void Huynya()
         {
-            textBox2.Text = Convert.ToString(oper);
-            textBox3.Text = Convert.ToString(operand);
+            textBox2.Text = Convert.ToString(oper + pr_operator);
+            textBox3.Text = Convert.ToString(operand + pr_operand);
+            int N1 = Convert.ToInt16(textBox2.Text);
+            int N2 = Convert.ToInt16(textBox3.Text);
+            textBox4.Text = Convert.ToString(pr_operator);
+            textBox5.Text = Convert.ToString(pr_operand);
+            int n1 = Convert.ToInt16(textBox4.Text);
+            int n2 = Convert.ToInt16(textBox5.Text);
+            textBox6.Text = Convert.ToString(N1 + N2);
+            textBox7.Text = Convert.ToString(n1 + n2);
+            int N = Convert.ToInt16(textBox6.Text);
+            int n = Convert.ToInt16(textBox7.Text);
+            D = (n1 / 2) * (N2 / n2);
+            L = 1/D;
+            textBox8.Text = Convert.ToString(L);
         }
     }
 }
